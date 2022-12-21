2749194dc76001678a4ebca374710e77e38f62b8
youssefhassan01 <youssef.hassan01@eng-st.cu.edu.eg>
2022-12-21 22:15:15 +0200

    Update Normalise64.v

--- Project/Normalise64.v
+++ Project/Normalise64.v
@@ -1,6 +1,6 @@
-module Normalise64(input wire [51:0] m, input wire [9:0] e, output wire [9:0] E, output wire[53:0] M);
+module Normalise64(input wire [51:0] m, input wire [9:0] e, output wire [11:0] E, output wire[53:0] M);
 
-assign E = e + 10'd1024;
-assign M = m + 54'd10000000000000000;
+assign E = e + 12'b010000000000;
+assign M = m + 55'd10000000000000000;
 
 endmodule 
