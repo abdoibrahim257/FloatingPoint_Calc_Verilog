d995726423828f12716a4d4a5a1f9c39be034aa6
youssefhassan01 <youssef.hassan01@eng-st.cu.edu.eg>
2022-12-24 20:33:36 +0200

    Updated Normaliser32

--- Project/Normalise32.v
+++ Project/Normalise32.v
@@ -1,10 +1,11 @@
 module Normalise32 ( input wire [22:0] A, input wire [22:0] B, input wire[7:0] eA, input wire[7:0] eB, 
-output wire [22:0] Am, output wire [22:0] Bm,input wire en,input wire load,input wire clk,input wire rst);
+output wire [22:0] Am, output wire [22:0] Bm, output wire OE,input wire en,input wire load,input wire clk,input wire rst);
 
 reg [22:0] Ai;
 reg [22:0] Bi;
 reg [7:0] eAi;
 reg [7:0] eBi;
+reg OEi;
 
 always@(posedge clk)
 begin
@@ -26,20 +27,31 @@ begin
 				if(eAi>eBi) begin
 					eBi <= eBi + 1;
 					Bi <= Bi >> 1;
+					OEi <= 1'b0;
 				end
 				else if(eBi>eAi)begin
 					eAi <= eAi + 1;
 					Ai <= Ai >> 1;
+					OEi <= 1'b0;
+				end
+				if(eBi == eAi) begin
+					Bi <= Bi;
+					Ai <= Ai;
+					eAi <= eAi;
+					eBi <= eBi;
+					OEi <= 1'b1;
 				end
 			end
 			else begin
 				if(eAi[7] == 1) begin
 					eAi <= eAi + 1;
 					Ai <= Ai >> 1;
+					OEi <= 1'b0;
 				end
 				else if(eBi[7] == 1) begin
 					eBi <= eBi + 1;
 					Bi <= Bi >> 1;
+					OEi <= 1'b0;
 				end
 			end
 		end
--- Project/fullAdder32.v
+++ Project/fullAdder32.v
@@ -1,4 +1,3 @@
-
 module fullAdder32 (input [22:0] A, input [22:0] B, input c_in, output [22:0] sum , output c_out);
 	
 	assign {c_out,sum} = A + B + c_in;
