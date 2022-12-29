6a08d710664fd11912f2dff7de45e4bbede77d4a
Abdo <abdulrahman.elhosseini01@eng-st.cu.edu.eg>
2022-12-30 01:43:34 +0200

    64bit components (Full Adder, Normaliser)

--- Project/Normalise64.v
+++ Project/Normalise64.v
@@ -1,6 +1,63 @@
-module Normalise64(input wire [51:0] m, input wire [10:0] e, output wire [10:0] E, output wire[53:0] M);
+module Normalise64 ( input wire [52:0] A, input wire [52:0] B, input wire[10:0] eA, input wire[10:0] eB, 
+output wire [52:0] Am, output wire [52:0] Bm, output wire OE,input wire en,input wire load,input wire clk,input wire rst);
 
-assign E = e + 11'd1023;
-assign M = m + 53'd10000000000000000;
+reg [52:0] Ai;
+reg [52:0] Bi;
+reg [10:0] eAi;
+reg [10:0] eBi;
+reg OEi;
 
-endmodule 
+always@(posedge clk)
+begin
+	if(rst) begin
+		Ai <= 0;
+		Bi <= 0;
+		eAi <= 0;
+		eBi <= 0;
+	end
+	else if(en) begin
+		if(load) begin
+			Ai <= A;
+			Bi <= B;
+			eAi <= eA;
+			eBi <= eB;
+		end
+		else begin
+			if(eAi[10] == eBi[10]) begin                         
+				if(eAi>eBi) begin
+					eBi <= eBi + 1;
+					Bi <= Bi >> 1;
+					OEi <= 1'b0;
+				end
+				else if(eBi>eAi)begin
+					eAi <= eAi + 1;
+					Ai <= Ai >> 1;
+					OEi <= 1'b0;
+				end
+				if(eBi == eAi) begin
+					Bi <= Bi;
+					Ai <= Ai;
+					eAi <= eAi;
+					eBi <= eBi;
+					OEi <= 1'b1;
+				end
+			end
+			else begin
+				if(eAi[10] == 1) begin
+					eAi <= eAi + 1;
+					Ai <= Ai >> 1;
+					OEi <= 1'b0;
+				end
+				else if(eBi[10] == 1) begin
+					eBi <= eBi + 1;
+					Bi <= Bi >> 1;
+					OEi <= 1'b0;
+				end
+			end
+		end
+	end
+end
+
+assign Am = Ai;
+assign Bm = Bi;
+endmodule	
--- Project/fullAdder64.v
+++ Project/fullAdder64.v
@@ -1,5 +1,45 @@
-module fullAdder64(input wire[53:0] A, input wire[53:0] B, input wire cin,output wire[53:0] S, output wire cout);
+module fullAdder64 (input wire rst,input wire clk, input wire load,input wire en, input wire PlusOrMinus,input wire [52:0] A, input wire [52:0] B, 
+input wire signA, input wire signB, input wire c_in, output wire [52:0] sum , output wire c_out);
+	
 
-assign {cout , S} = A + B + cin;
-
+	reg [52:0] Ai;
+	reg [52:0] Bi;
+	reg sA;
+	reg sB;
+	reg PlusOrMinusi;
+	always@(posedge clk) begin
+		if(rst == 1) begin
+			Ai<=0;
+			Bi<=0;
+			sA<=0;
+			sB<=0;
+		end
+		else begin
+			if(en == 1) begin
+				if(load == 1) begin
+					Ai<=A;
+					Bi<=B;
+					sA<=signA;
+					sB<=signB;
+					PlusOrMinusi<=PlusOrMinus; 
+				end
+				else begin
+					if(sA == 1) begin
+						Ai<= ~(Ai) + 1'b1;
+						sA<=1'b0;
+					end
+					if(sB == 1) begin
+						Bi<= ~(Bi) + 1'b1;
+						sB<=1'b0;					
+					end
+					else if(PlusOrMinusi == 1) begin
+						Bi<= ~(Bi) + 1'b1;
+						PlusOrMinusi <= 1'b0;
+					end
+				end
+			end
+		end
+	end
+	assign {c_out,sum} = (load == 0 && rst == 0) ? Ai + Bi + c_in : 0;
+	
 endmodule
