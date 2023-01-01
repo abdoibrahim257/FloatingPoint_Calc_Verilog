04251b93168c7625654e00d5311a626567cc6e8d
Ramy-Ashraf <ramy.abdelmaseeh01@eng-st.cu.edu.eg>
2023-01-01 05:03:04 +0200

    mul64 fixed

--- Project/mul64.v
+++ Project/mul64.v
@@ -8,10 +8,10 @@ module mul64 (
     output reg[63:0] result
   );
 
-reg [55:0] A_Mantissa,B_Mantissa;
-reg [54:0] Mantissa;
-reg [79:0] Temp_Mantissa;
-reg [39:0] A_Exponent,B_Exponent,Temp_Exponent,Exponent;
+reg [52:0] A_Mantissa,B_Mantissa;
+reg [51:0] Mantissa;
+reg [105:0] Temp_Mantissa;
+reg [11:0] A_Exponent,B_Exponent,Temp_Exponent,Exponent;
 reg A_sign,B_sign,Sign;
 
 
@@ -27,19 +27,20 @@ always @(posedge clk) begin
     end
     else if (en) begin
         if(load) begin
-            A_Mantissa <= {1'b1,A[54:0]};
-            B_Mantissa <= {1'b1,B[54:0]};
-            A_Exponent <= A[62:55];
-            B_Exponent <= B[62:55];
+            A_Mantissa <= {1'b1,A[51:0]};
+            B_Mantissa <= {1'b1,B[51:0]};
+            A_Exponent <= A[62:52];
+            B_Exponent <= B[62:52];
             A_sign <= A[63];
             B_sign <= B[63];
         end
         else begin
             
             Temp_Exponent = A_Exponent + B_Exponent - 1023;
+				
             Temp_Mantissa = A_Mantissa * B_Mantissa;
-            Mantissa = Temp_Mantissa[79] ? Temp_Mantissa[78:56] + 1'b1: Temp_Mantissa[77:55];
-            Exponent = Temp_Mantissa[79] ? Temp_Exponent + 1'b1 : Temp_Exponent;
+            Mantissa = Temp_Mantissa[105] ? Temp_Mantissa[104:53] + 1'b1: Temp_Mantissa[103:52];
+            Exponent = Temp_Mantissa[105] ? Temp_Exponent + 1'b1 : Temp_Exponent;
             Sign = A_sign^B_sign;
             result = {Sign,Exponent,Mantissa};
             
@@ -49,5 +50,5 @@ end
 
 
 endmodule
-///////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
